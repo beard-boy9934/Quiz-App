de8e19ddfcfcb68f49e02790ab274129a58fb97c
Sunny Kumar <kmr.sny913520@gmail.com>
2024-01-14 15:30:23 +0530

    first-commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://www.linkedin.com/in/sunnykumar9934/"
+        style={{ cursor: "pointer" }}
+      >
+        Beard-Boy
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
